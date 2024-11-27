430a92f3d084ccd2e3166d24653aaea363aef8ae
Anatoliy925 <anatolii.shokha.it.2021@lpnu.ua>
2024-11-28 00:49:38 +0200

    thank-you

---
+++ app/surveys/thank-you/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Thank = () => {
+  return (
+    <div>
+      <h1 style={{ color: "green", fontSize: "48px", textAlign: "center" }}>
+        THANK YOU!
+      </h1>
+      <p style={{ color: "darkgreen", fontSize: "24px", textAlign: "center" }}>
+        We appreciate your feedback and time.
+      </p>
+    </div>
+  );
+};
+
+export default Thank;
