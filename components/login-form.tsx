ce961216d56da3ea8568e1a3bce0c40714ba73fe
Anatoliy925 <anatolii.shokha.it.2021@lpnu.ua>
2024-11-28 00:52:12 +0200

    left just google sign in

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -19,53 +19,57 @@ export function LoginForm() {
     try {
       const provider = new GoogleAuthProvider();
       await signInWithPopup(auth, provider);
-      window.location.href = "/home";
+      window.location.href = "/dashboard";
     } catch (error) {
       console.error('Error signing in with Google:', error);
     }
   };
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
-            Login with Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="#" className="underline">
-            Sign up
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+    // <Card className="mx-auto max-w-sm">
+    //   <CardHeader>
+    //     <CardTitle className="text-2xl">Login</CardTitle>
+    //     <CardDescription>
+    //       Enter your email below to login to your account
+    //     </CardDescription>
+    //   </CardHeader>
+    //   <CardContent>
+    //     <div className="grid gap-4">
+    //       <div className="grid gap-2">
+    //         <Label htmlFor="email">Email</Label>
+    //         <Input
+    //           id="email"
+    //           type="email"
+    //           placeholder="m@example.com"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="grid gap-2">
+    //         <div className="flex items-center">
+    //           <Label htmlFor="password">Password</Label>
+    //           <Link href="#" className="ml-auto inline-block text-sm underline">
+    //             Forgot your password?
+    //           </Link>
+    //         </div>
+    //         <Input id="password" type="password" required />
+    //       </div>
+    //       <Button type="submit" className="w-full">
+    //         Login
+    //       </Button>
+    //       <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+    //         Login with Google
+    //       </Button>
+    //     </div>
+    //     <div className="mt-4 text-center text-sm">
+    //       Don&apos;t have an account?{" "}
+    //       <Link href="#" className="underline">
+    //         Sign up
+    //       </Link>
+    //     </div>
+    //   </CardContent>
+    // </Card>
+
+    <Button variant="outline" className="w-full" onClick={handleGoogleLogin}>
+    Login with Google
+    </Button>
   )
 }
